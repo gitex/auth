c143d10da81e8889ba1d6017e1365f55c7058482
Evgeniy Dorovatovskiy <eanegin@gmail.com>
2025-10-10 05:53:00 +0500

    chore: removed useless files

--- src/infra/claims/errors.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class TokenError(StrEnum):
-    REQUIRED_SUB = 'required_sub'
-    REQUIRED_JTI = 'required_jti'
-    REQUIRED_EXP = 'required_exp'
-    WRONG_ISSUER = 'wrong_issuer'
-    WRONG_AUDIENCE = 'wrong_audience'
